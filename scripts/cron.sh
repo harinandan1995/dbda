17cd021042111344b8166a93cd2048cb637866ca
Katam Harinandan Teja <hari.katam@tum.de>
2019-08-24 18:01:22 +0200

    - Added arguments for lrzsync

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,2 +1,4 @@
-rsync -Pav -e 'ssh -i my-openssh-key' ubuntu@"$1":/ssdtemp/checkpoints  /home/harikatam/TUM/sose2019/IDP/lrzsync/
-rsync -Pav -e 'ssh -i my-openssh-key' ubuntu@"$1":/ssdtemp/summaries  /home/harikatam/TUM/sose2019/IDP/lrzsync/
+echo 'Syncing summaries and checkpoints from lrzserver ' "$1"
+
+rsync -Pav -e "ssh -i $2" ubuntu@"$1":/ssdtemp/checkpoints  "$3"
+rsync -Pav -e "ssh -i $2" ubuntu@"$1":/ssdtemp/summaries  "$3"
--- scripts/lrzsync.sh
+++ scripts/lrzsync.sh
@@ -1,13 +1,22 @@
-ssh -i my-openssh-key ubuntu@"$1" 'mkdir -p /ssdtemp/datasets'
-ssh -i my-openssh-key ubuntu@"$1" 'mkdir -p /ssdtemp/checkpoints'
-ssh -i my-openssh-key ubuntu@"$1" 'mkdir -p /ssdtemp/summaries'
+ssh -i "$2" ubuntu@"$1" 'mkdir -p /ssdtemp/datasets'
+ssh -i "$2" ubuntu@"$1" 'mkdir -p /ssdtemp/checkpoints'
+ssh -i "$2" ubuntu@"$1" 'mkdir -p /ssdtemp/summaries'
 
-rsync -Pav -e 'ssh -i my-openssh-key' --exclude=env --exclude=.idea --exclude=summaries --exclude=checkpoints /home/harikatam/TUM/sose2019/IDP/building-design-assistant ubuntu@"$1":/home/ubuntu
-rsync -Pav -e 'ssh -i my-openssh-key' /home/harikatam/TUM/sose2019/IDP/datasets/tfrecords ubuntu@"$1":/ssdtemp/datasets
+rsync -Pav -e "ssh -i $2" --exclude=env --exclude=.idea --exclude=summaries --exclude=checkpoints "$3" ubuntu@"$1":/home/ubuntu
+rsync -Pav -e "ssh -i $2" "$5" ubuntu@"$1":/ssdtemp/datasets
 
-ssh -i my-openssh-key ubuntu@"$1" 'bash /home/ubuntu/building-design-assistant/scripts/lrzsetup.sh'
+ssh -i "$2" ubuntu@"$1" "bash /home/ubuntu/building-design-assistant/scripts/lrzsetup.sh"
 
-croncmd="bash /home/harikatam/TUM/sose2019/IDP/building-design-assistant/scripts/cron.sh $1"
-cronjob="0 */1 * * * $croncmd"
+croncmd="bash $3/scripts/cron.sh $1 $2 $4> $4/out.log"
+cronjob="*/1 * * * * $croncmd"
 
 ( crontab -l | grep -v -F "cron.sh" ; echo "$cronjob" ) | crontab -
+
+
+# Usage
+# 1st argument - ip of the lrz server
+# 2nd argument - path to the ssh key
+# 3rd argument - path to the repo
+# 4th argument - path to the folder where the output after training should be synced
+# 5th argument - path to the tfrecords folder
+# bash lrzsync.sh 10.195.2.3 ~/TUM/sose2019/IDP/lrzsync/my-openssh-key ~/TUM/sose2019/IDP/building-design-assistant ~/TUM/sose2019/IDP/lrzsync ~/TUM/sose2019/IDP/datasets/tfrecords
